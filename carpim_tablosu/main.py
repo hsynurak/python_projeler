255d36f7e6c1812a82b613d8d31219febc85b601
huseyinurak <huseyinurak@gmail.com>
2023-06-23 21:34:17 +0300

    descriptions added
    main.py descriptions and readme

--- carpim_tablosu/main.py
+++ carpim_tablosu/main.py
@@ -1,17 +1,23 @@
 from random import randint
 
+"""
+fonksiyonda girilen x ve y değerlerinin çarpımının sonucunun r değerine eşit olup olmadığını kontrol eder.
+r değerine eşit ise doğru mesajını verir
+eğer eşit değilse yanlış mesajı verir ve doğrusunu gösterir.
+"""
 def carpim(x,y,r):
-    dogru = 0
-    yanlis = 0
     result = str(x*y)
     if result == r:
-        dogru += 1
-        print("\t\t Cevap Doğru \n Karne: %s doğru, %s yanlış" %(dogru, yanlis))
+        print("\t\t Cevap Doğru ")
     else:
-        yanlis += 1
-        print("\t\t Cevap Yanlış, doğru cevap %s olacak. \n Karne: %s doğru, %s yanlış" %(result, dogru, yanlis))
+        print("\t\t Cevap Yanlış, doğru cevap %s olacak. " )
         
 
+"""
+fonksiyonda basla() fonksiyonu içerisinde seviye seçimi yapıldıktan sonra seçilen seviyeye göre random sayılar üretilir.
+bu random sayılar ile üretilen işlemlere cevap sorulur.
+seçilen seviyelerin her biri için 10 adet işlem sorulur. 10 işlem cevaplandıktan sonra tekrar seviye seçimi yapılmasına olanak sağlanır.
+"""
 def basla(range1, range2):
     for i in range(0,5):
         for j in range(0,5):
@@ -25,7 +31,10 @@ def basla(range1, range2):
                 seviye()
                 
             
-            
+"""
+Bu fonksiyon ile seviye seçilmesi istenir.
+Bu seviyeye göre basla() fonksiyonu uygun parametreler ile çağırılır.
+"""           
 def seviye():
     print("Seviye Seçiniz")
     print("Kolay seviye için 1")
---
+++ carpim_tablosu/readme.md
@@ -0,0 +1,5 @@
+Projede 3 adaet fonksiyon bulunmakta
+       carpim ==> çarpma işlemine girilen cevabın doğru olup olmadığına karar verir
+       basla  ==> Her seviye için toplam 10 örnek çözülmesine olanak sağlayan cevapların girdilerinin alındığı fonksiyon
+       seviye ==> Çarpma tablosundan sorular gelirken, çarpılan değerlerin seçildiği aralıkların seçildiği zorluk seçici
+
